198fee1befe64b0f8b490e4beec9ce96d2dc96e4
Flavio Spedaletti <flavio.hfs@gmail.com>
2021-07-23 18:04:21 -0300

    Add project files.

---
+++ DelegateAction-for-dummies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DelegateAction_for_dummies
+{
+    class Program
+    {
+        static Action hello = () => Console.WriteLine("Hello");
+
+        static void Main(string[] args)
+        {
+            hello += () => Console.WriteLine("Dummy");
+            hello += () => Console.WriteLine("Chaining actions like a boss 😎");
+
+            hello();
+        }
+    }
+}
